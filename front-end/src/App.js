68ed46cb4b3c8452c232edde9bc4494ef5219f09
Jens-Andreas Rensaa <jens.rensaa@gmail.com>
2017-04-10 18:37:44 +0100

    add function for adding rooms to server

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -36,12 +36,11 @@ let App = React.createClass({
             plantData: [],
             roomData: [],
             roomFilter: -1,
+            roomKeyTracker: 0,
         };
     },
 
     getPlantData(){
-
-
         var request = require('superagent');
         const self = this;
         request
@@ -54,17 +53,66 @@ let App = React.createClass({
     },
 
     getRoomData(){
-
-
         var request = require('superagent');
         const self = this;
         request
             .get(configData.serverConfig.baseUrl + configData.serverConfig.port + configData.serverConfig.roomEndpoint)
             .end(function (err, res) {
+
+                let keyTracker = 0;
+                for(let i = 0; i < res.body.length; i++){
+                    if(res.body[i].id > keyTracker){
+                        keyTracker = res.body[i].id;
+                    }
+                }
+
                 self.setState({
-                    roomData: res.body
+                    roomData: res.body,
+                    roomKeyTracker: keyTracker
                 });
             })
+
+
+    },
+
+    getPlantTypes(){
+        //TODO add this stuff
+    },
+
+    getWateringHistory(){
+        //TODO add this stuff
+    },
+
+    getSensorHistory(){
+        //TODO add this stuff
+    },
+
+    addRoomData(roomName){
+        this.state.roomKeyTracker++;
+
+        const roomData = {
+            id: this.state.roomKeyTracker,
+            name: roomName
+        }
+
+
+        //POST data to the server over the REST API.
+        var request = require('superagent');
+        const self = this;
+        request
+            .post(configData.serverConfig.baseUrl + configData.serverConfig.port + configData.serverConfig.roomEndpoint)
+            .send(roomData)
+            .end(function () {
+                self.getRoomData();
+            })
+    },
+
+    addPlantData(plantName,roomKey,plantType){
+        //TODO add post request for add plant
+    },
+
+    addPlantType(typeName, maxTemp, minTemp, maxMoisture, minMoisture){
+        //TODO add this stuff.
     },
 
 
@@ -72,7 +120,6 @@ let App = React.createClass({
     Called after the component is initalized, but right before it's rendered.
      */
     componentWillMount() {
-
         //Set the sidebar Props
         const mql = window.matchMedia(`(min-width: 1000px)`);
         mql.addListener(this.mediaQueryChanged);
@@ -81,14 +128,7 @@ let App = React.createClass({
             docked: mql.matches,
         });
 
-
-        /*GET plant data from server with superagent framework
-
-        Key concept: Call the GET to the server, then render the area. Since no data has been recieved yet,
-        a progress bar will be rendered instead of the plant-cards. Once data is recieved from the server, the
-        self.setState method is called, triggering the re-rendering of the area.
-
-         */
+        //Get the initial data from the server.
         this.getPlantData();
         this.getRoomData();
     },
@@ -117,6 +157,7 @@ let App = React.createClass({
     handleConfigureEvent(plantCardProps){
         console.log("Pressed the configure button on plant with id:" + plantCardProps.id);
         //Open configuration panel TODO: Make config planel
+        this.addRoomData("Magic Room");
     },
 
     handleWaterEvent(plantCardProps){
@@ -138,6 +179,7 @@ let App = React.createClass({
                 <span> My Plants</span>
             </span>
         );
+
 
         const sidebarContentProps = {
             filterCards: this.filterCards,
