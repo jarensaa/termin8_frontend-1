653ae78342e2432fe9d7d9c9627abb1b9e7948a4
Thea Svenkerud Rydjord <theasrydjord@dhcp-10-22-43-151.wlan.ntnu.no>
2017-04-20 11:34:06 +0200

    add floating button

---
+++ front-end/src/sidebar_content.js
@@ -0,0 +1,60 @@
+/**
+ * Created by Jens-Andreas on 04-Apr-17.
+ */
+import React from 'react';
+import Logo from './Termin8_logo.svg';
+
+const styles = {
+    sidebar: {
+        width: 256,
+        height: '100%',
+        backgroundColor: 'white',
+    },
+    sidebarLink: {
+        display: 'block',
+        padding: '16px 16px',
+        color: '#757575',
+        textDecoration: 'none',
+    },
+    divider: {
+        margin: '8px 0',
+        height: 1,
+        backgroundColor: '#757575',
+    },
+    content: {
+        padding: '16px',
+        height: '100%',
+        backgroundColor: 'white',
+    },
+    image: {
+        width: 200,
+        padding: '16px 16px',
+    },
+};
+
+const SidebarContent = (props) => {
+    const style = props.style ? {...styles.sidebar, ...props.style} : styles.sidebar;
+
+    const links = [];
+
+    for (let ind = 0; ind < 10; ind++) {
+        links.push(
+            <a key={ind} href="#" style={styles.sidebarLink}>Room {ind}</a>);
+    }
+
+    return (
+            <div style={style}>
+                <img src={Logo} style={styles.image}/>
+                <a href="index.html" style={styles.sidebarLink}>Home</a>
+                <div style={styles.divider} />
+                {links}
+            </div>
+
+    );
+};
+
+SidebarContent.propTypes = {
+    style: React.PropTypes.object,
+};
+
+export default SidebarContent;
